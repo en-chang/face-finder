0ab719b3436858422366905c7424077f7031aaf3
en-chang <enchangcs@gmail.com>
2021-06-07 03:15:35 -0500

    Created functions and styles to outline detected faces and added comments

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 import React from 'react';
 
+// API used to detect faces
 const app = new Clarifai.App({
   apiKey: '8ae99686d6794b1a8847f0ebdca3359b'
 });
 
+// Particle effect for background
 const particlesOptions = {
   "particles": {
     "number": {
@@ -37,31 +39,46 @@ class App extends React.Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
   }
 
+  // When text is input in ImageLinkForm
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
 
+  // When the Find button is pressed in ImageLinkForm
   onFindPress = () => {
     this.setState({imageUrl: this.state.input})
     app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-      .then(
-        function(response) {
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        },
-        function(err) {
-          // error
-        }
-      )
+      .then(response => this.displayBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
 
   render () {
-    const { input, imageUrl } = this.state;
+    const { input, imageUrl, box } = this.state;
     return (
       <div className="App">
         <Particles
@@ -74,7 +91,7 @@ class App extends React.Component {
           onInputChange={this.onInputChange} 
           onFindPress={this.onFindPress}
         />
-        <FaceRecognition imageUrl={imageUrl}/>
+        <FaceRecognition imageUrl={imageUrl} box={box}/>
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
         <img 
+          id='inputimage'
           alt='Face Recognition' 
           src={imageUrl}
           width='700px'
           height='auto'
+        />
+        <div 
+          className='bounding-box'
+          style={{
+            top: box.topRow, 
+            right: box.rightCol, 
+            bottom: box.bottomRow, 
+            left: box.leftCol
+          }}
         />
       </div>
     </div>
