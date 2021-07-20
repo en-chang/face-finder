5ded124a7492b1d572b6b2944cd7422cbc193d7f
en-chang <enchangcs@gmail.com>
2021-07-20 03:43:09 -0500

    Connect dropdown with icon

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { 
+  Dropdown, 
+  DropdownToggle, 
+  DropdownMenu, 
+  DropdownItem 
+} from 'reactstrap';
 
 class ProfileIcon extends React.Component {
   constructor(props) {
@@ -8,12 +14,31 @@ class ProfileIcon extends React.Component {
     }
   }
 
+  toggle = () => {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
   render() {
     return (
-      <div class="pa4 tc">
-        <img
-            src="http://tachyons.io/img/logo.jpg"
-            class="br-100 ba h3 w3 dib" alt="avatar" />
+      <div className="pa4 tc">
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle
+          tag="span"
+          data-toggle="dropdown"
+          aria-expanded={this.state.dropdownOpen}
+        >
+          <img
+              src="http://tachyons.io/img/logo.jpg"
+              className="br-100 ba h3 w3 dib" alt="avatar" 
+          />
+        </DropdownToggle>
+        <DropdownMenu className='b--transparent shadow-5' style={{marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}} >
+            <DropdownItem>View Profile</DropdownItem>
+            <DropdownItem>Sign Out</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </div>
     );
   }
